aaf0aa4335b38d4c36e18384476cd9491085a225
shreyansh0297 <s.limbachia.r@gmail.com>
2021-12-19 13:50:23 +0530

    first commit

---
+++ zerodhaclone/src/main/java/com/cg/zerodhaclone/ZerodhacloneApplication.java
@@ -0,0 +1,13 @@
+package com.cg.zerodhaclone;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZerodhacloneApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZerodhacloneApplication.class, args);
+	}
+
+}
---
+++ zerodhaclone/src/test/java/com/cg/zerodhaclone/ZerodhacloneApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cg.zerodhaclone;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZerodhacloneApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
